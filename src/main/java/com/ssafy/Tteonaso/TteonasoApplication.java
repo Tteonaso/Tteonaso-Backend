600308a98f03df6c73c0725ea894f5e0751c7707
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-11-18 11:56:52 +0900

    init commit

---
+++ src/main/java/com/ssafy/Tteonaso/TteonasoApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.Tteonaso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TteonasoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TteonasoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/Tteonaso/TteonasoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.Tteonaso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TteonasoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
