15f9e30ff2c00a03f1bd0e29417ea4e2d540af0e
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-11-19 14:04:20 +0900

    ✨ feat: MyBatis 연결

--- src/main/java/com/ssafy/Tteonaso/domain/Member.java
+++ src/main/java/com/ssafy/Tteonaso/domain/Member.java
@@ -2,9 +2,17 @@ package com.ssafy.Tteonaso.domain;
 
 import com.ssafy.Tteonaso.domain.enums.Gender;
 import com.ssafy.Tteonaso.domain.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Member {
     Long memberId;
     String name;
--- src/main/java/com/ssafy/Tteonaso/repository/MemberRepository.java
+++ src/main/java/com/ssafy/Tteonaso/repository/MemberRepository.java
@@ -1,4 +1,4 @@
 package com.ssafy.Tteonaso.repository;
 
-public class MemberRepository {
+public interface MemberRepository {
 }
