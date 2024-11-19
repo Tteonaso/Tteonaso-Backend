29fe9b4fbdb4274a128810e42fab3abc15f4130d
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-11-19 11:57:26 +0900

    ✨ feat: signup 컨트롤러 작성

--- src/main/java/com/ssafy/Tteonaso/web/controller/MemberRestController.java
+++ src/main/java/com/ssafy/Tteonaso/web/controller/MemberRestController.java
@@ -2,17 +2,24 @@ package com.ssafy.Tteonaso.web.controller;
 
 import com.ssafy.Tteonaso.apiPayload.ApiResponse;
 import com.ssafy.Tteonaso.converter.MemberConverter;
+import com.ssafy.Tteonaso.web.dto.MemberRequestDTO;
 import com.ssafy.Tteonaso.web.dto.MemberResponseDTO;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/member")
 public class MemberRestController {
 
     @GetMapping("/test")
     public ApiResponse<MemberResponseDTO.TempTestDTO> testAPI() {
         return ApiResponse.onSuccess(MemberConverter.toTempTestDTO());
+    }
+
+    @PostMapping("/signup")
+    public ApiResponse<MemberResponseDTO.SignUpResponseDTO> signUp(@ModelAttribute @Valid MemberRequestDTO.SignUpDTO signUpDTO) {
+        return ApiResponse.onSuccess(null);
     }
 }
--- src/main/java/com/ssafy/Tteonaso/web/dto/MemberRequestDTO.java
+++ src/main/java/com/ssafy/Tteonaso/web/dto/MemberRequestDTO.java
@@ -1,4 +1,19 @@
 package com.ssafy.Tteonaso.web.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
 public class MemberRequestDTO {
+
+    @Getter
+    @Setter
+    public static class SignUpDTO {
+        String name;
+        String email;
+        String password;
+        String gender;
+        String phone;
+        //MultipartFile profile_image;
+    }
 }
