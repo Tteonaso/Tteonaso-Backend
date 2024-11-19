0f40e6c12942652a83793c4b2d9f6fd695cace97
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-11-19 20:14:50 +0900

    ✨ feat: CustomUserDetailService

--- src/main/java/com/ssafy/Tteonaso/repository/MemberRepository.java
+++ src/main/java/com/ssafy/Tteonaso/repository/MemberRepository.java
@@ -4,6 +4,9 @@ package com.ssafy.Tteonaso.repository;
 import com.ssafy.Tteonaso.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
+    Optional<Member> findByEmail(String email);
 }
---
+++ src/main/java/com/ssafy/Tteonaso/service/CustomUserDetailService.java
@@ -0,0 +1,33 @@
+package com.ssafy.Tteonaso.service;
+
+import com.ssafy.Tteonaso.apiPayload.code.status.ErrorStatus;
+import com.ssafy.Tteonaso.apiPayload.exception.handler.MemberHandler;
+import com.ssafy.Tteonaso.domain.Member;
+import com.ssafy.Tteonaso.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return memberRepository.findByEmail(email)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new MemberHandler(ErrorStatus.MEMBER_NOT_FOUND));
+    }
+
+    private UserDetails createUserDetails(Member member) {
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRoles().toArray(new String[0]))
+                .build();
+    }
+}
