c873cc5b59f42f84a5a16ba43d20d8a469396ed8
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-11-19 12:06:01 +0900

    ✨ feat: member Domain 코드 작성

--- src/main/java/com/ssafy/Tteonaso/domain/Member.java
+++ src/main/java/com/ssafy/Tteonaso/domain/Member.java
@@ -1,4 +1,20 @@
 package com.ssafy.Tteonaso.domain;
 
+import com.ssafy.Tteonaso.domain.enums.Gender;
+import com.ssafy.Tteonaso.domain.enums.Status;
+
+import java.time.LocalDateTime;
+
 public class Member {
+    Long memberId;
+    String name;
+    String password;
+    //Address address;
+    Gender gender;
+    String email;
+    String phone;
+    String profileImage;
+    LocalDateTime createdAt;
+    LocalDateTime modifiedAt;
+    Status status;
 }
---
+++ src/main/java/com/ssafy/Tteonaso/domain/enums/Status.java
@@ -0,0 +1,5 @@
+package com.ssafy.Tteonaso.domain.enums;
+
+public enum Status {
+    ACTIVE, INACTIVE, DORMANT;
+}
