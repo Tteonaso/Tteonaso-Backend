16c72f4850f491cca90e1385fac8ebe74cceb21f
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-11-20 11:19:56 +0900

    init commit

--- src/main/java/com/ssafy/Tteonaso/web/controller/MemberRestController.java
+++ src/main/java/com/ssafy/Tteonaso/web/controller/MemberRestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/member")
+@CrossOrigin(origins = "http://localhost:5173")
 public class MemberRestController {
     private final MemberService memberService;
 
