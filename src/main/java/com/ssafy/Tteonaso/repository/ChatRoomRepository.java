8390369dd3334de9a7457203650a97232f21f047
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-11-20 15:23:15 +0900

    ✨ feat: ChatRoomRepository 작성

---
+++ src/main/java/com/ssafy/Tteonaso/repository/ChatRoomRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.Tteonaso.repository;
+
+import com.ssafy.Tteonaso.domain.ChatRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+}
--- src/main/java/com/ssafy/Tteonaso/service/ChatRoomServiceImpl.java
+++ src/main/java/com/ssafy/Tteonaso/service/ChatRoomServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ssafy.Tteonaso.service;
 
 import com.ssafy.Tteonaso.domain.ChatRoom;
+import com.ssafy.Tteonaso.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +10,11 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class ChatRoomServiceImpl implements ChatRoomService{
+
+    public final ChatRoomRepository chatRoomRepository;
+
     @Override
     public List<ChatRoom> readAllChatRoom() {
-        return null;
+        return chatRoomRepository.findAll();
     }
 }
