85c1c6904b2f56338d1e3916dc286b5e1f42c458
황인준 <os2864@naver.com>
2024-11-25 09:12:06 +0900

    :recycle: refactor: 프로그램 실행 시 TimeZone 지정

--- src/main/java/com/ssafy/Tteonaso/TteonasoApplication.java
+++ src/main/java/com/ssafy/Tteonaso/TteonasoApplication.java
@@ -1,13 +1,28 @@
 package com.ssafy.Tteonaso;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableScheduling
+@RequiredArgsConstructor
+@Slf4j
 public class TteonasoApplication {
+	private final Environment environment;
 
 	public static void main(String[] args) {
+		init();
 		SpringApplication.run(TteonasoApplication.class, args);
 	}
 
+	private static void init() {
+		log.info("Spring Server TimeZone : Asia/Seoul");
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
